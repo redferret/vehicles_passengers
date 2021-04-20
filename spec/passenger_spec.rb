9dd85fd30790c8c765ca18cc768ccde6a0f58a16
Richard <rdesilvey@gmail.com>
2021-04-20 13:08:31 -0600

    Test for attributes

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -10,5 +10,23 @@ describe Passenger do
       })
       expect(passenger).to be_a Passenger
     end
+
+    it 'has a name' do
+      passenger = Passenger.new({
+        name: 'Charlie',
+        age: 18
+      })
+
+      expect(passenger.name).to eq 'Charlie'
+    end
+
+    it 'has an age' do
+      passenger = Passenger.new({
+        name: 'Charlie',
+        age: 18
+      })
+
+      expect(passenger.age).to eq 18
+    end
   end
 end
