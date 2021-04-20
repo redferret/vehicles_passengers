e8954a0b4f20dfec11ef33b33c0eb67148e8c8e6
Richard <rdesilvey@gmail.com>
2021-04-20 13:06:31 -0600

    Modify test for attributes

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -4,7 +4,10 @@ require './lib/passenger'
 describe Passenger do
   describe '#new' do
     it 'creates an instance of Passenger' do
-      passenger = Passenger.new
+      passenger = Passenger.new({
+        name: 'Charlie',
+        age: 18
+      })
       expect(passenger).to be_a Passenger
     end
   end
