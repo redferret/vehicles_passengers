e58c6c5d8f80a8b386bfcdd9173821bf6ea8b7cf
Richard <rdesilvey@gmail.com>
2021-04-20 14:00:45 -0600

    Refactor Example Group for '#new'

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,29 +3,28 @@ require './lib/vehicle'
 
 describe Vehicle do
   describe '#new' do
-    it 'creates a new instance of Vehicle' do
-      vehicle = Vehicle.new('2001', 'Honda', 'Civic')
-      expect(vehicle).to be_a Vehicle
+    before :each do
+      @vehicle = Vehicle.new('2001', 'Honda', 'Civic')
+    end
+
+    it 'creates a new instance of @vehicle' do
+      expect(@vehicle).to be_a @vehicle
     end
 
     it 'has a year' do
-      vehicle = Vehicle.new('2001', 'Honda', 'Civic')
-      expect(vehicle.year).to eq '2001'
+      expect(@vehicle.year).to eq '2001'
     end
 
     it 'has a make' do
-      vehicle = Vehicle.new('2001', 'Honda', 'Civic')
-      expect(vehicle.make).to eq 'Honda'
+      expect(@vehicle.make).to eq 'Honda'
     end
 
     it 'has a model' do
-      vehicle = Vehicle.new('2001', 'Honda', 'Civic')
-      expect(vehicle.model).to eq 'Civic'
+      expect(@vehicle.model).to eq 'Civic'
     end
 
     it 'has passengers' do
-      vehicle = Vehicle.new('2001', 'Honda', 'Civic')
-      expect(vehicle.passengers).to eq []
+      expect(@vehicle.passengers).to eq []
     end
   end
 
@@ -53,7 +52,7 @@ describe Vehicle do
       allow(vehicle).to receive(:add_passenger)
 
       passenger_mock = double('Passenger')
-      
+
       vehicle << passenger_mock
 
       expect(vehicle).to have_received(:add_passenger).with(passenger_mock)
